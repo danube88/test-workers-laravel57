c5e7ddb824580d5e6a713081dfc509ba6f036d35
danube88 <36491066+danube88@users.noreply.github.com>
2019-01-23 23:34:02 +0300

    added package laravelcollective/html
    v5.7.1

--- config/app.php
+++ config/app.php
@@ -174,7 +174,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        // Form&Html
+        Collective\Html\HtmlServiceProvider::class,
+        //
     ],
 
     /*
@@ -223,7 +225,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        //Form&Html
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        //
     ],
 
 ];
