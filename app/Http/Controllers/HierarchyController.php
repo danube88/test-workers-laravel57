8f291072ba7f451f2b75f525c8c5699c57fd6395
danube88 <36491066+danube88@users.noreply.github.com>
2019-01-31 19:56:05 +0300

    lazy loading for employee tree.
    ленивая загрузка для дерево сотрудников.

--- app/Http/Controllers/HierarchyController.php
+++ app/Http/Controllers/HierarchyController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Response;
+use Validator;
 use DB;
 
 class HierarchyController extends Controller
@@ -12,11 +14,11 @@ class HierarchyController extends Controller
     {
       // code...
       if(view()->exists('hierarchy')){
-
-      $workers = DB::table('workers as w')
+        $workers = DB::table('workers as w')
                 ->leftJoin('positions as p', 'p.id', '=', 'w.position_id')
                 ->leftJoin('subordinations as s', 's.subordinate_id', '=', 'w.id')
                 ->leftJoin('subordinations as sb', 'sb.head_id', '=', 'w.id')
+                ->where('s.head_id','=',null)
                 ->select([
                   "w.id",
                   DB::raw("CONCAT(w.surname,' ',w.name,' ',w.patronymic) as nameWorker"),
@@ -28,9 +30,60 @@ class HierarchyController extends Controller
                   "s.head_id",
                   DB::raw("COUNT(sb.subordinate_id) as count")
                 ])->groupBy(['w.id','s.head_id','w.surname','w.name','w.patronymic','w.reception_date','w.salary','p.id','p.level','p.name_position'])->get();
-        return view('hierarchy',['workers' => $workers]);
+        foreach ($workers as $value) {
+          $array[] = $value->id;
+        }
+        $workers_two = DB::table('workers as w')
+                  ->leftJoin('positions as p', 'p.id', '=', 'w.position_id')
+                  ->leftJoin('subordinations as s', 's.subordinate_id', '=', 'w.id')
+                  ->leftJoin('subordinations as sb', 'sb.head_id', '=', 'w.id')
+                  ->whereIn('s.head_id',$array)
+                  ->select([
+                    "w.id",
+                    DB::raw("CONCAT(w.surname,' ',w.name,' ',w.patronymic) as nameWorker"),
+                    "w.reception_date",
+                    "w.salary",
+                    "p.id as id_p",
+                    "p.level",
+                    "p.name_position",
+                    "s.head_id",
+                    DB::raw("COUNT(sb.subordinate_id) as count")
+                  ])->groupBy(['w.id','s.head_id','w.surname','w.name','w.patronymic','w.reception_date','w.salary','p.id','p.level','p.name_position'])->get();
+          $workers = array_merge($workers->toArray(), $workers_two->toArray());
+          return view('hierarchy',['workers' => $workers]);
       } else {
         abort(404);
+      }
+    }
+
+    public function dataHierarchy(Request $request)
+    {
+      $input = $request->except('_token');
+
+      $rules = [
+              'id' => 'required|numeric'
+              ];
+      $validator = Validator::make($input, $rules);
+      if ($validator->fails()) {
+        return Response::json(array('errors' => $validator->getMessageBag()->toArray()));
+      } else {
+        $workers = DB::table('workers')
+              ->leftJoin('positions', 'positions.id', '=', 'workers.position_id')
+              ->leftJoin('subordinations', 'subordinations.subordinate_id', '=', 'workers.id')
+              ->where('subordinations.head_id','=',$input['id'])
+              ->get(array('workers.id',
+                          DB::raw("CONCAT(workers.surname,' ',workers.name,' ',workers.patronymic) as nameWorker"),
+                          'positions.name_position',
+                          'subordinations.head_id'
+                        ));
+
+        $node = array();
+        foreach ($workers as $worker) {
+          $subordination = DB::table('subordinations')->where('subordinations.head_id','=',$worker->id)->count();
+          $isFolder = ($subordination != 0)?1:0;
+          $node[] = '{ "id": "'.$worker->id.'","title": "<h6> '.$worker->name_position.'</h6>'.$worker->nameWorker.'","isFolder": "'.$isFolder.'"}';
+        }
+        return Response::json($node);
       }
     }
 }
--- resources/views/hierarchy.blade.php
+++ resources/views/hierarchy.blade.php
@@ -20,16 +20,30 @@
 @section('content')
 <div class="container">
   <ul class="list-group Container" id="tree">
-  @foreach ($workers as $worker)
-    @if($worker->head_id == null )
-      <li id="{{ $worker->id }}" class="list-group-item Node IsRoot IsLast ExpandOpen">
-        <div class="fa fa-minus-square-o fa-lg Expand"></div>
-        <div class="Content">{!! '<h6>'.$worker->name_position.'</h6>'.$worker->nameWorker!!}</div>
-        <ul class="list-group Container">
-        </ul>
-      </li>
-    @endif
-  @endforeach
+    @foreach ($workers as $worker)
+      @if($worker->head_id == null)
+        <li id="{{ $worker->id }}" class="list-group-item Node IsRoot IsLast {{ ($worker->count != 0) ? 'ExpandOpen':'ExpandLeaf' }}">
+        @if($worker->count >= 1)
+          <div class="fa fa-minus-square-o fa-lg Expand"></div>
+        @else
+          <div class="fa fa-minus fa-lg Expand"></div>
+        @endif
+          <div class="Content"><h6>{!! $worker->name_position !!}</h6>{!! $worker->nameWorker !!}</div>
+          <ul class="list-group Container">
+            @foreach ($workers as $value)
+              @if($value->head_id == $worker->id)
+              <li id="{{ $value->id }}" class="list-group-item Node {{ ($value->count != 0) ? 'ExpandClosed':'ExpandLeaf' }}">
+                <div class="fa {{ ($value->count != 0) ? 'fa-plus-square-o':'fa-minus' }} fa-lg Expand"></div>
+                <div class="Content"><h6>{!! $value->name_position !!}</h6>{!! $value->nameWorker !!}</div>
+                <ul class="list-group Container">
+                </ul>
+              </li>
+              @endif
+            @endforeach
+          </ul>
+        </li>
+      @endif
+    @endforeach
   </ul>
 </div>
 @endsection
@@ -39,77 +53,139 @@
 
 @push('scripts')
 <script>
-  var obj = {!! $workers !!};
-
-  Object.keys(obj).forEach(function(key) {
-    if (this[key].head_id != null) {
-      var elem = document.getElementById(this[key].head_id);
-      var li = document.createElement('li');
-      li.id = this[key].id;
-
-      var count = this[key].count;
-
-      if(count >= 1) {
-        li.className = "list-group-item Node Expand" + 'Closed';
-        //
-        li.innerHTML = '<div class="fa fa-plus-square-o fa-lg Expand"></div><div class="Content">'+'<h6>'+this[key].name_position+'</h6>'+this[key].nameWorker+'</div>';
-      } else {
-        li.className = "list-group-item Node Expand" + 'Leaf';
-        //
-        li.innerHTML = '<div class="fa fa-minus fa-lg Expand"></div><div class="Content">'+'<h6>'+this[key].name_position+'</h6>'+this[key].nameWorker+'</div>';
-      };
-
-      if (count != 0) {
-        li.innerHTML += '<ul class="list-group Container"></ul>';
-      }
-      elem.getElementsByClassName('Container')[0].appendChild(li);
-    }
-  }, obj);
-
   $(document).ready(function() {
+
     $(".hierarchy").addClass('active');
+
+    var element = document.getElementById('tree');
+    var url = '{{ route("dataHierarchy") }}';
 
     function hasClass(elem, className) {
       return new RegExp("(^|\\s)"+className+"(\\s|$)").test(elem.className);
-    };
+    }
 
     function toggleNode(node) {
-      // определить новый класс для узла
+      // {{--определить новый класс для узла--}}
       var newClass = hasClass(node, 'ExpandOpen') ? 'ExpandClosed' : 'ExpandOpen';
       var element = node.getElementsByTagName('div')[0];
       var newElem = hasClass(element, 'fa-minus-square-o') ? 'fa-plus-square-o' : 'fa-minus-square-o';
-      // заменить текущий класс на newClass
-      // находим отдельно стоящий open|close и меняет на newClass
+      // {{--заменить текущий класс на newClass
+      // регексп находит отдельно стоящий open|close и меняет на newClass--}}
       var re =  /(^|\s)(ExpandOpen|ExpandClosed)(\s|$)/;
       node.className = node.className.replace(re, '$1'+newClass+'$3');
       var de =  /(^|\s)(fa-minus-square-o|fa-plus-square-o)(\s|$)/;
       element.className = element.className.replace(de, '$1'+newElem+'$3');
+    }
+
+    function load(node) {
+
+      function showLoading(on) {
+        var expand = node.getElementsByTagName('div')[0];
+        expand.className = on ? 'fa fa-spinner fa-lg ExpandLoading' : 'fa fa-minus-square-o fa-lg Expand';
+      }
+
+      function onSuccess(data) {
+        if (!data.errcode) {
+          onLoaded(data);
+          showLoading(false);
+        } else {
+          showLoading(false);
+          onLoadError(data);
+        }
+      }
+
+      function onAjaxError(xhr, status){
+        showLoading(false);
+        var errinfo = { errcode: status }
+        if (xhr.status != 200) {
+          // {{--может быть статус 200, а ошибка
+          // из-за некорректного JSON--}}
+          errinfo.message = xhr.statusText;
+        } else {
+          errinfo.message = 'Некорректные данные с сервера';
+        }
+        onLoadError(errinfo);
+      }
+
+      function onLoaded(data) {
+        var i = 0;
+        i = data.length;
+        $.each(data, function(key, val) {
+          var child = JSON.parse(val);
+
+          var li = document.createElement('li');
+          li.id = child.id;
+
+          if(child.isFolder == 1) { li.className = "list-group-item Node Expand" + 'Closed';
+            li.innerHTML = '<div class="fa fa-plus-square-o fa-lg Expand"></div><div class="Content">'+child.title+'</div>'
+          } else {
+            li.className = "list-group-item Node Expand" + 'Leaf';
+            li.innerHTML = '<div class="fa fa-minus fa-lg Expand"></div><div class="Content">'+child.title+'</div>'
+          };
+          if (key == i-1) { li.className += ' IsLast' };
+
+          //if (child.isFolder) {
+            li.innerHTML += '<ul class="list-group Container"></ul>';
+          //};
+          node.getElementsByTagName('ul')[0].appendChild(li);
+        });
+
+        node.isLoaded = true;
+        toggleNode(node);
+      }
+
+      function onLoadError(error) {
+        var msg = "Ошибка "+error.errcode;
+        if (error.message) msg = msg + ' :'+error.message;
+        alert(msg);
+      }
+
+      showLoading(true);
+
+      $.ajax({
+        url: url,
+        type: 'GET',
+        data: {'id': node.id},
+        dataType: 'JSON',
+        success: onSuccess,
+        error: onAjaxError,
+        cache: false
+      });
     };
 
-    document.getElementById('tree').onclick = function(event) {
+    element.onclick = function(event) {
       event = event || window.event;
       var clickedElem = event.target || event.srcElement;
 
       if (!hasClass(clickedElem, 'Expand')) {
-        return; // клик не там
+        return; // {{--клик не там--}}
       };
 
-      // Node, на который кликнули
+      //{{--Node, на который кликнули--}}
       var node = clickedElem.parentNode;
 
       if (hasClass(node, 'ExpandLeaf')) {
-        return; // клик на листе
+        return; //{{--клик на листе--}}
       };
 
       if (node.isLoaded || node.getElementsByTagName('li').length) {
+        //{{--Узел уже загружен через AJAX(возможно он пуст)--}}
         toggleNode(node);
         return;
       };
 
       if (node.getElementsByTagName('li').length) {
+        //{{-- Узел не был загружен при помощи AJAX, но у него почему-то есть потомки
+        // Например, эти узлы были в DOM дерева до вызова tree()
+        // Как правило, это "структурные" узлы
+        // ничего подгружать не надо--}}
         toggleNode(node);
         return;
       };
+
+        //{{--загрузить узел--}}
+      load(node);
+
     };
   });
 </script>
